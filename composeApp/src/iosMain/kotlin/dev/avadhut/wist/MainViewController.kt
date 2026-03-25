32182d2fb416729c1cce06a07e1710a4ce340a6f
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2026-03-25 22:57:10 +0530

    feat: set main App component as root view for iOS

--- composeApp/src/iosMain/kotlin/dev/avadhut/wist/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/dev/avadhut/wist/MainViewController.kt
@@ -1,11 +1,10 @@
 package dev.avadhut.wist
 
 import androidx.compose.ui.window.ComposeUIViewController
-import dev.avadhut.wist.ui.screens.ComponentDemoScreen
 import dev.avadhut.wist.ui.theme.WistTheme
 
 fun MainViewController() = ComposeUIViewController {
     WistTheme {
-        ComponentDemoScreen()
+        App()
     }
 }
